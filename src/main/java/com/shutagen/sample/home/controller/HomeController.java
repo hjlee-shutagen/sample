48ce3ddb8be5ad33ae730256c440fb7adb36c21b
golch <golchmaster51@gmail.com>
2025-01-09 15:17:29 +0900

    add 2 APIs (health check and hello)

--- src/main/java/com/shutagen/sample/home/controller/HomeController.java
+++ src/main/java/com/shutagen/sample/home/controller/HomeController.java
@@ -1,4 +1,17 @@
 package com.shutagen.sample.home.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Sample server is Healthy!!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
