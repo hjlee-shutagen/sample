b57580aa645984090bc3a1f9bdb609ac87852964
golch <golchmaster51@gmail.com>
2025-01-16 14:30:59 +0900

    logback 설정 추가

--- src/main/java/com/shutagen/sample/home/controller/HomeController.java
+++ src/main/java/com/shutagen/sample/home/controller/HomeController.java
@@ -1,24 +1,29 @@
 package com.shutagen.sample.home.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
     @GetMapping("/")
     public String home() {
+        log.info("Health Check Request Received");
         return "Sample server is Healthy!!";
     }
 
     @GetMapping("/hello")
     public String getHello() {
+        log.info("Hello World");
         return "Hello World";
     }
 
     @PostMapping("/hello")
     public String postHello(@RequestParam String name) {
+        log.info("Hello {}", name);
         return "Hello " + name;
     }
 }
